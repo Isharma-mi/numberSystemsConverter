e47425d639f00b5638f620269b02c6d1ee4f4980
ishaa <ishaa@isharma.attlocal.net>
2024-04-04 20:49:21 -0400

    Power method
    - Added method to calculate power of a base number to its exponent
    	- Added as of now for binary to decimal conversion

--- src/com/example/NumberSystemsConverter/logic/Converter.java
+++ src/com/example/NumberSystemsConverter/logic/Converter.java
@@ -3,7 +3,7 @@ package com.example.NumberSystemsConverter.logic;
 public class Converter {
 	// Gets a binary num as a string and converts it to be a decimal number
 	public int binaryToDecimal(String binaryNum) {
-		// Array that will contain each digit of the binaryNum
+		// Array that will contain each digit of the binaryNum, not needed but keep in case wanna show steps later
 		byte[] arrayOfDigits = new byte[binaryNum.length()];
 		
 		// Break down binaryNum input into separate digits stored in arrayOfDigits
@@ -17,7 +17,28 @@ public class Converter {
 			System.out.println(arrayOfDigits[i]);
 		}
 		
+		// Loop thru byte[] and convert each to an int
+		int powerTracker = 0;
+		for (int i = arrayOfDigits.length - 1; i >= 0; i--) {
+			int digitToDecimal = arrayOfDigits[i] * (int) Math.pow(2, powerTracker++);
+		}
+		
 		// TODO: Change to be actual converted number
 		return -1;
+	}
+	
+	/* 
+	 * Helper method to calculate a number raised to a power
+	 * Done to take less time than Math.pow() and to avoid data conversion loss
+	 * Don't need to verify input since its a helper method, just need to make sure it receives valid arguments
+	 */
+	public int power(int base, int exponent) {
+		// Initialize to the base
+		int result = 1;
+		// Start from 0 so if exponent is 0 won't loop
+		for (int i = 0; i < exponent; i++) {
+			result *= base ;
+		}
+		return result;
 	}
 }
