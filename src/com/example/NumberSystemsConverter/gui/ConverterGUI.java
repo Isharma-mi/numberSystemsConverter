31d19aa0f858919fc2af4b40a0e39703fdb02c65
Ishaan Sharma <107057912+Isharma-mi@users.noreply.github.com>
2024-04-23 15:48:22 -0400

    Dropdown menu for selecting conversion
    - Dropdown menu works
    - Put placeholders for actual implementation
    - NEED TO: Get conversion to actually work

--- src/com/example/NumberSystemsConverter/gui/ConverterGUI.java
+++ src/com/example/NumberSystemsConverter/gui/ConverterGUI.java
@@ -1,11 +1,15 @@
 package com.example.NumberSystemsConverter.gui;
 
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.Parent;
+
 
 public class ConverterGUI {
 	
@@ -22,6 +26,11 @@ public class ConverterGUI {
 		
 		// For components on the center
 		Button convertButton = new Button("Convert!");
+		String[] conversionsArray = {"Binary to Decimal", "Decimal to Binary", "Octal to Binary"};
+		ChoiceBox<String> conversionsChoiceBox = new ChoiceBox<String>(FXCollections.observableArrayList(conversionsArray));
+		
+		VBox centerBox = new VBox();
+		centerBox.getChildren().addAll(convertButton, conversionsChoiceBox);
 		
 		// For components on the right side
 		Label resultLabel = new Label("Conversion result:");
@@ -34,14 +43,31 @@ public class ConverterGUI {
 		// For combining all components
 		BorderPane layout = new BorderPane();
 		layout.setLeft(leftBox);
-		layout.setCenter(convertButton);
+		layout.setCenter(centerBox);
 		layout.setRight(rightBox);
 		
 		// For when button is clicked
 		convertButton.setOnMouseClicked((event) -> {
-			convertedResultField.setText("Conversion will go here");
+			// Gets the selected item from dropdown menu by its index
+			int conversionSelected = conversionsChoiceBox.getSelectionModel().getSelectedIndex();
+			
+			// To perform conversion specified by user
+			switch (conversionSelected) {
+				case 0:
+					// TODO: Implement binary to decimal conversion here
+					break;
+				case 1:
+					// TODO: Implement decimal to binary conversion here
+					break;
+				case 2:
+					// TODO: Implement octal to binary conversion here
+					break;
+				default:
+					// If user tries to convert with no option selected
+					convertedResultField.setText("ERROR: Please select a conversion");
+			}
 		});
-		
+			
 		return layout;
 	}
 }
--- src/module-info.java
+++ src/module-info.java
@@ -6,5 +6,6 @@
  */
 module NumberSystemsConverter {
 	requires javafx.controls;
+	requires javafx.base;
 	exports com.example.NumberSystemsConverter.logic;
 }
